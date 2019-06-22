c58669deacbbb8142a7373705189f754d769911f
Ringo Huang <huang.ringo1994@gmail.com>
2019-06-21 18:14:39 -0700

    Fix output folder path

--- Functions/GUI_DefineOutputFullPathName.m
+++ Functions/GUI_DefineOutputFullPathName.m
@@ -1,13 +1,16 @@
-function [app,outputFullPathName] = GUI_DefineOutputFullPathName(app,p,output_fieldnames)
+function [app, outputFullPathName] = GUI_DefineOutputFullPathName(app, p, output_fieldnames)
 % Defines OutputFullPathName
 %   Automatically generates the folder if output folder edit field is
 %   empty; otherwise, uses the value in output folder edit field.
 
-output_string_pairs = GUI_CreateStringPairs(p,output_fieldnames);  %only include the fieldnames that you want in the output filename
+output_string_pairs = GUI_CreateStringPairs(p, output_fieldnames);  %only include the fieldnames that you want in the output filename
 
 % If the OutputFolderEditField is empty, automatically fill it in
 if isempty(app.OutputFolderEditField.Value)                 % automatically generates output folder path name if the field is empty
-    app.OutputFolderEditField.Value = fullfile(fileparts(mfilename('fullpath')),'Data',app.ExperimentNameEditField.Value);
+    current_filename = mfilename('fullpath');
+    current_filename_split = strsplit(current_filename,filesep);
+    parent_directory = fullfile(current_filename_split{1:end-2});
+    app.OutputFolderEditField.Value = fullfile(parent_directory,'Data',app.ExperimentNameEditField.Value);
 end
 
 % If Output Folder doesn't exist, create it
