b1455a77d929a3fa7b465fd214dd973a0d7c6f0f
Ringo Huang <huang.ringo1994@gmail.com>
2019-06-27 18:43:31 -0700

    handle error where Gazepoint control is not open; fix auto-assigned output path

--- Functions/Client2_ConnectToGP3.m
+++ Functions/Client2_ConnectToGP3.m
@@ -1,8 +1,7 @@
 function session_client = Client2_ConnectToGP3(app, varargin)
 % Client 2 refers to the Matlab session responsible for recording the GP3
-% data stream.
+% data stream (e.g., the GUI)
 %
-% 5/10/2019 - includes progress dialog option
 %
 % This function sets up the socket between session 2 client and GP3
 % server and configures the data stream
@@ -26,7 +25,30 @@ end
 %% Set up session 2 set up address and port, and configure socket properties
 session_client = tcpip('127.0.0.1', 4242);
 session_client.InputBufferSize = 100000;
-fopen(session_client);
+
+%% Try to open connect with GP3 server; if unable to connect, prompt user to turn on Gazepoint Control (or check port) or to cancel
+not_connected = 1;
+
+while not_connected
+    try
+        fopen(session_client);
+        not_connected = 0;
+    catch ME
+        fclose(session_client);
+        switch ME.identifier
+            case 'instrument:fopen:opfailed'
+                retry_or_cancel = uiconfirm(app.UIFigure,...
+                    'Could not connect to GP3. Make sure Gazepoint Control is open and GP3 Port Number is set to 4242, then press retry.',...
+                    'Could not connect','Options',{'Retry','Cancel'},...
+                    'DefaultOption',2,'CancelOption',2,'Icon','error');
+                if strcmp(retry_or_cancel,'Cancel')
+                    rethrow(ME);
+                end
+            otherwise
+                rethrow(ME);
+        end
+    end
+end
 session_client.Terminator = 'CR/LF';
 
 %% Close progress_dlg
--- Functions/GUI_DefineOutputFullPathName.m
+++ Functions/GUI_DefineOutputFullPathName.m
@@ -1,16 +1,31 @@
 function [app, outputFullPathName] = GUI_DefineOutputFullPathName(app, p, output_fieldnames)
-% Defines OutputFullPathName
-%   Automatically generates the folder if output folder edit field is
-%   empty; otherwise, uses the value in output folder edit field.
+% Assigns an output directory for the gazepoint data if the
+% OutputFolderEditField is empty. The assigned directory is
+% ..\Data\app_name. If the assigned/specified output folder does not exist,
+% automatically creates the directory. 
+% 
+% User can specify a different output folder even after connection with GP3
+% is established. Subsequent "Start Experiment" runs will output Gazepoint
+% data to the new output folder.
+%
+% Arguments:
+%   output_fieldnames - in the main app GUI code, the output_fieldnames are
+%   pre-defined based on which name-pair parameters I think should be
+%   included in the Gazepoint filename
+%
+% Created by: Ringo Huang
+% Date: 6/20/2019
 
 output_string_pairs = GUI_CreateStringPairs(p, output_fieldnames);  %only include the fieldnames that you want in the output filename
 
-% If the OutputFolderEditField is empty, automatically fill it in
-if isempty(app.OutputFolderEditField.Value)                 % automatically generates output folder path name if the field is empty
-    current_filename = mfilename('fullpath');
-    current_filename_split = strsplit(current_filename,filesep);
-    parent_directory = fullfile(current_filename_split{1:end-2});
-    app.OutputFolderEditField.Value = fullfile(parent_directory,'Data',app.ExperimentNameEditField.Value);
+% If the OutputFolderEditField is empty, automatically assigns an output
+% directory in ..\Data\app_name
+if isempty(app.OutputFolderEditField.Value)                 
+    current_fullpath = mfilename('fullpath');
+    current_fullpath_split = strsplit(current_fullpath,filesep);
+    parent_directory = fullfile(current_fullpath_split{1:end-2});
+    app_name = regexprep(app.UIFigure.Name, ' Task', '');   % Remove ' task' from the app name string
+    app.OutputFolderEditField.Value = fullfile(parent_directory,'Data',app_name);
 end
 
 % If Output Folder doesn't exist, create it
@@ -27,5 +42,5 @@ while exist(outputFullPathName,'file')
     outputFullPathName = fullfile(app.OutputFolderEditField.Value,outputFileName);
     file_num = file_num + 1;
 end
-end
+
 
